bf30d67ecaa556cd637b79acf14f5e99df22409d
nimamehranfar <mehranfarnima@rocketmail.com>
2025-10-28 14:58:17 +0100

    US3 red

--- test/test_translator.py
+++ test/test_translator.py
@@ -13,3 +13,19 @@ class TestPigLatinTranslator(TestCase):
         translator = PigLatinTranslator("")
         translation = translator.translate()
         self.assertEqual("nil", translation)
+
+    def test_translate_starting_vowel_ending_y(self):
+        translator = PigLatinTranslator("any")
+        translation = translator.translate()
+        self.assertEqual("anynay", translation)
+
+    def test_translate_starting_vowel_ending_vowel(self):
+        translator = PigLatinTranslator("apple")
+        translation = translator.translate()
+        self.assertEqual("appleyay", translation)
+
+    def test_translate_starting_vowel_ending_consonant(self):
+        translator = PigLatinTranslator("ask")
+        translation = translator.translate()
+        self.assertEqual("askay", translation)
+
